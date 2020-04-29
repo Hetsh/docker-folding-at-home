18605390876d7af6b905362eaa9bdda773a15828
Hetsh <git@hetsh.de>
2020-04-29 17:42:15 +0200

    Import

---
+++ Dockerfile
@@ -0,0 +1,34 @@
+FROM library/debian:stable-20200422-slim
+RUN DEBIAN_FRONTEND="noninteractive" && \
+    apt-get update && \
+    apt-get install --assume-yes \
+        bzip2=1.0.6-9.2~deb10u1 \
+        ca-certificates=20190110
+
+# App user
+ARG APP_USER="fah"
+ARG APP_UID=1362
+RUN useradd --uid "$APP_UID" --user-group --no-create-home --shell /sbin/nologin "$APP_USER"
+
+# Folding@Home package
+ARG FAH_DIR="/opt/folding-at-home"
+ARG FAH_ARCHIVE="fah.tar.bz2"
+ARG ARCHIVE_URL="https://download.foldingathome.org/releases/public/release/fahclient/debian-stable-64bit/v7.6/fahclient_7.6.9-64bit-release.tar.bz2"
+ADD "$ARCHIVE_URL" "$FAH_DIR/$FAH_ARCHIVE"
+RUN tar --extract --directory "$FAH_DIR" --file "$FAH_DIR/$FAH_ARCHIVE" --strip-components=1 && \
+    apt-get purge -y bzip2 && \
+    rm -r "$FAH_DIR/$FAH_ARCHIVE" /var/lib/apt/lists /var/cache/apt
+ENV PATH="$FAH_DIR:$PATH"
+
+# Volumes
+ARG DATA_DIR="/fah"
+RUN mkdir "$DATA_DIR" && \
+    chown -R "$APP_USER":"$APP_USER" "$DATA_DIR"
+VOLUME ["$DATA_DIR"]
+
+#      WEB      CONTROL
+EXPOSE 7396/tcp 36330/tcp
+
+USER "$APP_USER"
+WORKDIR "$DATA_DIR"
+ENTRYPOINT ["FAHClient"]
---
+++ update.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+
+# Abort on any error
+set -e -u
+
+# Simpler git usage, relative file paths
+CWD=$(dirname "$0")
+cd "$CWD"
+
+# Load helpful functions
+source libs/common.sh
+source libs/docker.sh
+
+# Check dependencies
+assert_dependency "jq"
+assert_dependency "curl"
+
+# Debian Stable
+IMG_CHANNEL="stable"
+update_image "library/debian" "Debian" "false" "$IMG_CHANNEL-\d+-slim"
+
+# Folding@Home has folders for major versions, requiring two stage version scrape
+FAH_VERSION_REGEX="(\d+\.)+\d+"
+BASE_URL="https://download.foldingathome.org/releases/public/release/fahclient/debian-stable-64bit"
+MAJOR_FAH_VERSION=$(curl -L -s "$BASE_URL" | grep -P -o "v$FAH_VERSION_REGEX" | sort --version-sort | tail -n 1)
+
+# Folding@Home client
+CURRENT_FAH_VERSION="${_CURRENT_VERSION%-*}"
+NEW_FAH_VERSION=$(curl -L -s "BASE_URL/$NEW_FAH_VERSION" | grep -P -o "$FAH_VERSION_REGEX" | sort --version-sort | tail -n 1)
+if [ "$CURRENT_FAH_VERSION" != "$NEW_FAH_VERSION" ]; then
+	prepare_update "ARCHIVE_URL" "Folding@Home" "$CURRENT_FAH_VERSION" "$NEW_FAH_VERSION"
+	update_version "$NEW_TS_VERSION"
+
+	# Since the minecraft server is not a regular package, the version number needs
+	# to be replaced with the url to download the binary
+	_UPDATES[-3]="BIN_URL"
+	_UPDATES[-2]="\".*\""
+	_UPDATES[-1]="\"$BASE_URL/$MAJOR_FAH_VERSION/fahclient_$NEW_FAH_VERSION-64bit-release.tar.bz2\""
+fi
+
+# Packages
+PKG_URL="https://packages.debian.org/$IMG_CHANNEL/amd64"
+update_pkg "lib32gcc1" "32bit GCC libs" "false" "$PKG_URL" "\d+:(\d+\.)+\d+-\d+"
+update_pkg "ca-certificates" "CA-Certificates" "false" "$PKG_URL" "\d{8}"
+
+if ! updates_available; then
+	echo "No updates available."
+	exit 0
+fi
+
+# Perform modifications
+if [ "${1-}" = "--noconfirm" ] || confirm_action "Save changes?"; then
+	save_changes
+
+	if [ "${1-}" = "--noconfirm" ] || confirm_action "Commit changes?"; then
+		commit_changes
+	fi
+fi
